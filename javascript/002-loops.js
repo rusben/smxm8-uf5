3ab067ea34ed84a78a4fe0c8d6dac6783a4b177d
Rubén <rusben@gmail.com>
2018-05-07 16:06:50 +0200

    002 basic

--- javascript/002-loops.js
+++ javascript/002-loops.js
@@ -3,8 +3,15 @@ var elements = [3, 7, 9, 18, 24, 5, 7, 27, 32, 35, 42, 47];
 // Retorna la suma de los números que son estrictamente mayores que 18
 function greaterThan18(elements) {
 
-  // Escriviu aquí el vostre codi:
-  return 0;
+  var total = 0;
+
+  for (i = 0; i < elements.length; i++) {
+    if (elements[i] > 18) {
+      total = total + elements[i];
+    }
+  }
+
+  return total;
 }
 
 document.getElementById("A").innerHTML = greaterThan18(elements);
@@ -14,8 +21,15 @@ document.getElementById("A").innerHTML = greaterThan18(elements);
 // y que además son pares
 function greaterThan18Odd(elements) {
 
-  // Escriviu aquí el vostre codi:
-  return 0;
+  var total = 0;
+
+  for (i = 0; i < elements.length; i++) {
+    if ((elements[i] > 18) && (elements[i] % 2 == 0)) {
+      total = total + elements[i];
+    }
+  }
+
+  return total;
 }
 
 document.getElementById("B").innerHTML = greaterThan18Odd(elements);
@@ -24,9 +38,42 @@ document.getElementById("B").innerHTML = greaterThan18Odd(elements);
 // y que además son pares y que se encuentran en posiciones impares del vector
 function greaterThan18OddEvenPositions(elements) {
 
-  // Escriviu aquí el vostre codi:
-  return 0;
+  var total = 0;
+
+  for (i = 0; i < elements.length; i++) {
+
+    if ((elements[i] > 18) &&
+        (elements[i] % 2 == 0) &&
+        (i % 2 == 1)) {
+      total = total + elements[i];
+    }
+
+  }
+
+  return total;
 }
 
 
 document.getElementById("C").innerHTML = greaterThan18OddEvenPositions(elements);
+
+// Retorna la multiplicación de los números que son estrictamente mayores que 18
+// y que además son pares y que se encuentran en posiciones pares del vector
+function multiplyAll(elements) {
+
+  var total = 0;
+
+  for (i = 0; i < elements.length; i++) {
+
+    if ((elements[i] > 18) &&
+        (elements[i] % 2 == 0) &&
+        (i % 2 == 1)) {
+      total = total + elements[i];
+    }
+
+  }
+
+  return total;
+}
+
+
+document.getElementById("D").innerHTML = multiplyAll(elements);
