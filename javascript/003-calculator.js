70c2ad4d6060782ca58ce6676352b343c6fc3ca6
Rusben <rusben@gmail.com>
2018-05-07 14:33:00 +0200

    Calculator

---
+++ javascript/003-calculator.js
@@ -0,0 +1,19 @@
+function sum() {
+  var operandA = document.getElementById("operand-a");
+  var operandB = document.getElementById("operand-b");
+  var result = document.getElementById("result");
+
+  result.value = Number(operandA.value) + Number(operandB.value);
+}
+
+function sub() {
+
+}
+
+function mul() {
+
+}
+
+function div() {
+
+}
