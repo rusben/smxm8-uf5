27b08dde462472bb93b51469c5aa87fc16ad5114
Rusben <rusben@gmail.com>
2018-05-08 12:58:15 +0200

    Full calculator

--- javascript/005-full-calculator.js
+++ javascript/005-full-calculator.js
@@ -1,19 +1,38 @@
 var calcButtons = document.getElementsByClassName("calc-btn");
+var formula = document.getElementById("formula");
 
-console.log(calcButtons);
+var equalButton = document.getElementById("btn-equal");
+var resetButton = document.getElementById("btn-reset");
 
-Array.prototype.forEach.call(calcButtons, function(el) {
-    el.addEventListener("click", wow);
-});
+// Array.prototype.forEach.call(calcButtons, function(el) {
+//     el.addEventListener("click", formulaAddValue);
+// });
 
 // Or
 
 [].forEach.call(calcButtons, function (el) {
-  el.addEventListener("click", wow);
+  el.addEventListener("click", formulaAddValue);
 });
 
+equalButton.addEventListener("click", formulaEval);
+resetButton.addEventListener("click", formulaReset);
 
-function wow() {
-    console.log("wow");
-    console.log(this.value);
+function formulaAddValue() {
+    // console.log(this.value);
+    // console.log(formula);
+    formula.value = formula.value + this.value;
 }
+
+function formulaEval() {
+  formula.value = eval(formula.value);
+}
+
+function formulaReset() {
+  formula.value = "";
+}
+
+
+
+
+
+//console.log(calcButtons);
