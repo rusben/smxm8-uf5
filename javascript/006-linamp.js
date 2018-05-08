d12c0a7322eda1b7ec71bf8a688f6c9d1ef9db33
Rusben <rusben@gmail.com>
2018-05-08 13:42:09 +0200

    linamp chooser

--- javascript/006-linamp.js
+++ javascript/006-linamp.js
@@ -2,14 +2,23 @@ var track = document.getElementById("track");
 var playButton = document.getElementById("btn-play");
 var pauseButton = document.getElementById("btn-pause");
 var stopButton = document.getElementById("btn-stop");
+var fileButton = document.getElementById("btn-file");
 
-playButton.addEventListener("dblclick", play);
+playButton.addEventListener("click", play);
 pauseButton.addEventListener("click", pause);
 stopButton.addEventListener("click", stop);
+fileButton.addEventListener("change", changeFile);
 
 function play() { track.play(); }
+
 function pause() { track.pause(); }
+
 function stop() {
   pause();
   track.currentTime = 0;
  }
+
+function changeFile() {
+  //console.log(this.value);
+  track.src = this.value;
+}
