ccb41995d2b9209689acd3719ba104c4efb83d6b
rusben <rusben@gmail.com>
2018-05-16 10:21:57 +0200

    Canvas + components

--- javascript/016-game-canvas.js
+++ javascript/016-game-canvas.js
@@ -1,10 +1,4 @@
-function startGame() {
-    myGameArea.start();
-}
-
-function pauseGame() {
-    myGameArea.pause();
-}
+var myGamePiece;
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
@@ -13,8 +7,63 @@ var myGameArea = {
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
     },
     pause : function() {
         console.log("pause");
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 600, 120);
+}
+
+function pauseGame() {
+    myGameArea.pause();
+}
+
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+
+function moveright() {
+    myGamePiece.speedX += 1;
 }
